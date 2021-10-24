fdeb9bd0cb53ad04b4e3d4332d2e9ac4a845889e
yamadaitsuki <yamada351035@gmail.com>
2021-10-24 19:52:30 +0900

    高階関数を追加
    react-hands-on-learning2-3 3章:JavaScriptにおける関数型プログラミング

--- chapter3/関数型プログラム.js
+++ chapter3/関数型プログラム.js
@@ -20,3 +20,17 @@ const appendAMPM = clockTime => ({
   ...clockTime,
   ampm: clockTime.hours >= 12 ? 'PM' : 'AM',
 });
+
+// 関数targetを受け取り、その関数を使って時刻を表示する
+const display = target => time => target(time);
+
+const formatClock = format => time =>
+  format.replace('hh', time.hours).
+    replace('mm', time.minutes).
+    replace('ss', time.seconds).
+    replace('ampm', time.ampm);
+
+const prependZero = key => clockTime => ({
+  ...clockTime,
+  [key]: clockTime[key] < 10 ? '0' + clockTime[key] : '' + clockTime[key],
+});
