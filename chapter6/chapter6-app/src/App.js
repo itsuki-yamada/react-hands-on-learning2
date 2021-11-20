2e2d086d7cc1d2631d3f36d58afa85b764d2e5db
yamadaitsuki <yamada351035@gmail.com>
2021-11-20 21:44:09 +0900

    6-3-1 制御されていないコンポーネント（uncontrolled component)の追加
    react-hands-on-learning2-6 #6章:ステート管理

--- chapter6/chapter6-app/src/App.js
+++ chapter6/chapter6-app/src/App.js
@@ -1,24 +1,30 @@
 import {useState} from 'react';
 import colorData from './color-data.json';
 import ColorList from './components/ColorList';
+import AddColorForm from './components/AddColorForm';
 
 function App() {
   const [colors, setColors] = useState(colorData);
   return (
-      <ColorList
-          colors={colors}
-          onRateColor={(id, rating) => {
-            const newColors = colors.map(color =>
-                color.id === id ? {...color, rating} : color,
-            );
-            setColors(newColors);
-          }
-          }
-          onRemoveColor={id => {
-            const newColors = colors.filter(color => color.id !== id);
-            setColors(newColors);
-          }}
-      />);
+      <>
+        <AddColorForm/>
+        <hr/>
+        <ColorList
+            colors={colors}
+            onRateColor={(id, rating) => {
+              const newColors = colors.map(color =>
+                  color.id === id ? {...color, rating} : color,
+              );
+              setColors(newColors);
+            }
+            }
+            onRemoveColor={id => {
+              const newColors = colors.filter(color => color.id !== id);
+              setColors(newColors);
+            }}
+        />
+      </>
+  );
 }
 
 export default App;
