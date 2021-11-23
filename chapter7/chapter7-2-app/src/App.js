530348d9b4d7211f5f65e7898554fcaf0c526ada
yamadaitsuki <yamada351035@gmail.com>
2021-11-23 13:35:27 +0900

    7-2 useLayoutEffectの使用例
    react-hands-on-learning2-7 #7章:フック

--- chapter7/chapter7-2-app/src/App.js
+++ chapter7/chapter7-2-app/src/App.js
@@ -1,4 +1,26 @@
-import {useEffect, useLayoutEffect} from 'react';
+import {useEffect, useLayoutEffect, useState} from 'react';
+
+/**
+ * 描画前に必要になるwidthとheightを取得する
+ * @returns {number[]}
+ */
+function useWindowSize() {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const resize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener('resize', resize);
+    resize();
+    return () => window.removeEventListener('resize', resize);
+  });
+
+  return [width, height];
+}
 
 function App() {
   useEffect(() => console.log('useEffect'));
