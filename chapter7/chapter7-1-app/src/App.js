19431ddf7248180b528726efbe2a3ef83fdbdd64
yamadaitsuki <yamada351035@gmail.com>
2021-11-23 12:35:37 +0900

    7-1-2　検証用フック作成
    react-hands-on-learning2-7 #7章:フック

--- chapter7/chapter7-1-app/src/App.js
+++ chapter7/chapter7-1-app/src/App.js
@@ -1,6 +1,12 @@
 import Checkbox from './components/Checkbox';
+import {useAnyKeyToRender} from './hooks/useAnyKeyToRender';
+import {useEffect} from 'react';
 
 function App() {
+  useAnyKeyToRender();
+  useEffect(() => {
+    console.log('fresh render');
+  });
   return (
       <>
         <Checkbox/>
