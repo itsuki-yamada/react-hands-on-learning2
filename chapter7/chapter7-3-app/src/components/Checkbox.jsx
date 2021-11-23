7ba0327bb9d0dafed9ba80d37706d66bccb1d280
yamadaitsuki <yamada351035@gmail.com>
2021-11-23 14:11:16 +0900

    useReducerを使ったリファクタリング
    reducerとは、現在のステートを受け取って新しいステート値を返す関数のこと。
    
    react-hands-on-learning2-7 #7章:フック

--- chapter7/chapter7-3-app/src/components/Checkbox.jsx
+++ chapter7/chapter7-3-app/src/components/Checkbox.jsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 
 const Checkbox = () => {
-  const [checked, setChecked] = useState(false);
+  const [checked, toggle] = useReducer(checked => !checked, false);
   return (
       <>
         <input type="checkbox"
                value={checked}
-               onChange={() => setChecked(checked => !checked)}
+               onChange={toggle}
         />
         {checked ? 'checked' : 'not checked'}
       </>
