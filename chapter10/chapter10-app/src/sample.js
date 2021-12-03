b4fb2ae569a59711a2b7a180745d92f98c379987
yamadaitsuki <yamada351035@gmail.com>
2021-12-03 23:20:50 +0900

    10章: Prettierの導入
    ESLint: コードの正しさを保証する
    Prettier: コードの可読性を保証する
    react-hands-on-learning2-10 #10 章:テスト

--- chapter10/chapter10-app/src/sample.js
+++ chapter10/chapter10-app/src/sample.js
@@ -1,11 +1,1 @@
-const gnar = 'gnarly';
-const info = ({
-  file = __filename, dir = __dirname,
-}) => (<p>
-      {dir}: {file} </p>
-);
-switch (gnar) {
-  default:
-    console.log('gnarly');
-    break;
-}
+console.log("Prettier TEST");
